117f55ce9d93b68e8ef5e267c0447d1c5677d138
Mihail Vorobyev <1viklover@gmail.com>
2022-08-22 19:21:58 +0500

    update project

---
+++ HARRIS_SET_TIME/DCA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HARRIS_SET_TIME
+{
+    public class Dca {
+        public Dca(dynamic config)
+        {
+            user = config.user;
+            password = config.password;
+            number = config.number;
+        }
+        public string user { get; set; }
+        public string password { get; set; }
+        public string number { get; set; }
+    }
+}
